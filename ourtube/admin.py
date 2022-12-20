e192d400798b06e969e7b45aefbbada0cdb045dd
Smile <smailbestybay@gmail.com>
2022-12-19 22:02:44 -0800

    make proper migrations without syncdb, register models to admin

--- ourtube/admin.py
+++ ourtube/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, YoutubeChannel, Feed, Membership
 
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
+admin.site.register(YoutubeChannel)
+admin.site.register(Feed)
+admin.site.register(Membership)
--- ourtube/models.py
+++ ourtube/models.py
@@ -5,3 +5,19 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+    def __str__(self):
+        return self.username
+
+class YoutubeChannel(models.Model):
+    pass
+
+class Feed(models.Model):
+    members = models.ManyToManyField(User, through="Membership")
+    channels = models.ManyToManyField(YoutubeChannel)
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    is_owner = models.BooleanField()
