0080fbbd2f5c9e43f731dbeb8f07f6b2f0d828de
Smile <smailbestybay@gmail.com>
2022-12-20 20:55:51 -0800

    add youtube channel inline to feed in admin

--- ourtube/admin.py
+++ ourtube/admin.py
@@ -7,12 +7,16 @@ class MembershipInline(admin.TabularInline):
     model = Membership
     extra = 1
 
+class YoutubeChannelInline(admin.TabularInline):
+    model = Feed.channels.through
+    extra = 1
+
 class MemberUserAdmin(UserAdmin):
     '''extend UserAdmin to add membership inline to admin page'''
     inlines = (MembershipInline,)
 
 class FeedAdmin(admin.ModelAdmin):
-    inlines = (MembershipInline,)
+    inlines = (MembershipInline, YoutubeChannelInline)
 # Register your models here.
 
 admin.site.register(User, MemberUserAdmin)
